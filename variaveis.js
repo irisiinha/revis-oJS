a5ff7e9fa975135dbfd3ab19d803977dbd2c944d
irisiinha <irismeiresilvaoliveira@gmail.com>
2025-03-25 14:11:30 +0000

    BLA

---
+++ variaveis.js
@@ -0,0 +1,9 @@
+var nome = "Íris"
+let idade = 16
+const signo = "gêmeos"
+
+/*
+O "var" permite você criar variaveis com o mesmo nome, mas alterando dependendo da linha que foi escrita.
+O "let" permite criar também uma variavel, mas diferente do "var" ela não permite criar mais de uma variavel com o mesmo nome.
+O "const" cria uma variavel que não pode ser alterada, ou seja, uma variavel constante.
+*/
